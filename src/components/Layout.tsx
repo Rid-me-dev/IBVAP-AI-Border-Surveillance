08bed704b58c9c80f061d081a9aeb753a5478413
Riddhi Bhattacharyya <sonuriddhi200@gmail.com>
2026-09-07 04:12:38 +0530

    Import useEffect in Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
